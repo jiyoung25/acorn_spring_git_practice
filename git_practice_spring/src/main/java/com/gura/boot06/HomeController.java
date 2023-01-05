956c85f88ea6bbe835314bcf5b219926267981ec
shinwoocheol <swc9704@gmail.com>
2023-01-05 12:22:58 +0900

    1. home.jsp 에서 신우철 입니다. 에 a태그 추가 2. woocheol_form.jsp 만들고 HomeController에 woocheolForm() 추가

--- git_practice_spring/src/main/java/com/gura/boot06/HomeController.java
+++ git_practice_spring/src/main/java/com/gura/boot06/HomeController.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
@@ -19,5 +21,10 @@ public class HomeController {
 		request.setAttribute("noticeList", noticeList);
 		
 		return "home";
+	}
+	
+	@RequestMapping("/woocheol_form")
+	public String woocheolForm() {
+		return "woocheol_form";
 	}
 }
